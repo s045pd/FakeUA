4d7f39f7618a48c996f1d797d5c408c027876d38
ai3 <aoii103@126.com>
2018-11-06 17:37:56 +0800

    init

---
+++ FakeUA.py
@@ -0,0 +1,198 @@
+
+
+import json
+import logging
+import math
+import os
+import re
+import sys
+import time
+from pprint import pprint
+from urllib.parse import quote, urljoin
+
+import asks
+import trio
+from pyquery import PyQuery as jq
+from stem import Signal
+from stem.connection import connect
+from termcolor import colored
+
+from FakeUAdb import UAS
+
+asks.init('trio')  # 初始化trio
+FAKEHEADER = {
+    "x-requested-with": "XMLHttpRequest",
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36",
+    "referer": "http://www.mafengwo.cn/",
+    "accept": "text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01"
+}  # 默认伪造headers
+POOLS = {}  # 数据池
+TASKS = set()  # 任务池
+DATANUMS = 0
+MAXNUMS = 0
+LIMIT = 1  # 并发限制
+PERPAGE = 50
+spiderSession = asks.Session(connections=LIMIT)
+spiderSession.headers = FAKEHEADER
+spiderSession.timeout_manager
+
+
+logging.basicConfig(
+    format="[%(asctime)s] >>> %(levelname)s  %(name)s: %(message)s", level=logging.INFO)  # 初始化日志输出格式，级别
+loger = logging.getLogger('FakeUA')  # 初始化一个日志对象
+try:
+    controller = connect()
+    controller.authenticate()
+except Exception as e:
+    loger.error(colored('请检测您的Tor端口','red'))
+    exit()
+
+
+async def getTypesL1():
+    """
+        取得一级分类
+    """
+    url = "https://developers.whatismybrowser.com/useragents/explore/"
+    resp = await spiderSession.get(url)
+    # listing-by-field-name > li:nth-child(1) > h2 > a
+    # listing-by-field-name > li:nth-child(2) > h2 > a
+    async with trio.open_nursery() as nursery:
+        for item in jq(resp.text)("#listing-by-field-name > li > h2 > a").items():
+            types = item.text().strip().replace(' ', '_').lower()
+            POOLS[types] = {}
+            nursery.start_soon(
+                getTypesL2, POOLS[types], types, urljoin(url, item.attr('href')))
+
+async def getTypesL2(target, types, href):
+    """
+        取得二级分类
+    """
+    loger.info(colored(f'fetching {href}', 'yellow'))
+    resp = await spiderSession.get(href)
+    async with trio.open_nursery() as nursery:
+        for item in jq(resp.text)("body > div.content-base > section > div > table > tbody > tr").items():
+            name = item(
+                'td:nth-child(1)>a').text().strip().replace(' ', '_').lower()
+            target[name] = {}
+            url = urljoin(href, item('td:nth-child(1)>a').attr('href'))
+            nums = int(item('td:nth-child(2)').text().strip())
+            target[name]['url'] = url
+            target[name]['nums'] = nums
+            target[name]['UA_list'] = []
+            for page in range(1, math.ceil(nums/PERPAGE)+1):
+                TASKS.add('__'.join([
+                    types,
+                    name,
+                    f"{url}{page}"
+                ]))
+
+async def getUAs():
+    global MAXNUMS
+    """
+        爬行任务调度
+    """
+    limit = trio.CapacityLimiter(LIMIT)
+    while TASKS:
+        MAXNUMS = len(list(TASKS))
+        loger.info(colored(f'当前任务量:{MAXNUMS}', 'red'))
+        await trio.sleep(1)
+        async with trio.open_nursery() as nursery:
+            for item in list(TASKS):
+                nursery.start_soon(getUAsitem, item, limit)
+
+async def getUAsitem(detals, limit):
+    global DATANUMS
+    global MAXNUMS
+
+    """
+        获取单个任务
+    """
+    types, name, url = detals.split('__')
+    target = POOLS[types][name]['UA_list']
+    async with limit:
+        try:
+            loger.info(colored(f'fetching -> {url}', 'yellow'))
+            resp = await spiderSession.get(url, timeout=5, retries=3)
+            for item in jq(resp.text)(
+                    "body > div.content-base > section > div > table > tbody > tr").items():
+                datas = {
+                    # 'uid':'',
+                    'useragent': item('td.useragent').text(),
+                    # 'href': item('td.useragent>a').attr('href'),
+                    'software': item('td:nth-child(2)').attr('title'),
+                    'engine': item('td:nth-child(3)').text(),
+                    'types': item('td:nth-child(4)').text(),
+                    'popularity': item('td:nth-child(5)').text()
+                }
+                loger.info(
+                    '[' +
+                    colored(DATANUMS,'green')+
+                    '/'+
+                    colored(MAXNUMS,'yellow')+
+                    '/'+
+                    colored(str(len(target)), 'blue') +
+                    ']' +
+                    colored('->', 'blue').join([
+                        colored(types, 'red'),
+                        colored(name, 'red'),
+                        colored(datas["useragent"], 'green')
+                    ]))
+                target.append(datas)
+            TASKS.remove(detals)
+            DATANUMS +=1
+            MAXNUMS -=1
+        except KeyboardInterrupt:
+            raise
+        except Exception as e:
+            loger.error(colored(e, 'red'))
+            NewID()
+
+def NewID():
+    controller.signal(Signal.NEWNYM)
+    loger.error(colored('切换线路', 'red'))
+
+def SaveJson(datas, filename):
+    """
+        Json数据存储
+    """
+    if not datas:
+        return
+    loger.info(colored(f'文件存储至 {filename}', 'yellow'))
+    with open(filename, 'w') as f:
+        f.write(json.dumps(datas, indent=4, ensure_ascii=False))
+
+def MakeChunk(datas,length=100):
+    for item in range(1, math.ceil(len(datas)/length)):
+        yield datas[item*length:(item+1)*length]
+
+def SaveToDB(datas,model):
+    data_source = []
+    for tk, td in datas.items():
+        for nk,nd in td.items():
+            data_source.extend(nd['UA_list'])
+    if data_source:
+        loger.info(colored(f'数据存储至DB中 [{len(data_source)}]', 'yellow'))
+        for chunk in list(MakeChunk(data_source)):
+            model.insert_many(data_source).execute()
+
+def main():
+    """
+        主逻辑
+    """
+    while True:
+        try:
+            trio.run(getTypesL1)
+            break
+        except Exception:
+            NewID()
+    trio.run(getUAs)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
+    except Exception as e:
+        loger.error(colored(e, 'red'))
+    finally:
+        SaveToDB(POOLS, UAS)
---
+++ FakeUAdb.py
@@ -0,0 +1,62 @@
+import hashlib
+
+from peewee import *
+
+db = SqliteDatabase('useragents.db') # 初始化数据库
+
+
+class UAS(Model):
+    uid = AutoField(primary_key=True, null=True) # 自增ID
+    useragent = TextField() # useragent
+    software = CharField(null=True) # 软件类型
+    engine = CharField(null=True) # 引擎
+    types = CharField(null=True) # 硬件类型
+    popularity = CharField(null=True) # 通用性
+
+    class Meta:
+        database = db # 指定数据库
+
+db.connect() # 连接数据库
+db.create_tables([UAS]) # 初始化创建不存在的库
+
+
+def UserAgent(searchwords, methods='and'):
+    """
+        {
+            "key":[
+                "words1",
+                "words2"
+            ]
+        }
+    """
+    count = 0
+    resagent = ''
+    if methods not in ['and', 'or']:
+        return ''
+    methods = '&' if not methods == 'or' else '|'
+    whereQuery = f' {methods} '.join([
+        f'(UAS.{key} << {str(item)})' for key, item in searchwords.items()
+    ])
+    try:
+        count = UAS.select().where(eval(whereQuery)).order_by(fn.Random()).count()
+        resagent = UAS.select().where(eval(whereQuery)).order_by(
+            fn.Random()).limit(1)[0].useragent
+    except Exception as e:
+        pass
+    return count, resagent
+
+
+def UserAgentGroups(colname, limit=10):
+    if colname in ['software', 'engine', 'types', 'popularity']: # 判定查询字段是否合法
+        target = eval(f'UAS.{colname}') # 取得目标字段类
+        return {eval(f'item.{colname}'): item.nums for item in UAS.select(target, fn.COUNT(target).alias('nums')).group_by(target).order_by(fn.COUNT(target).desc()).limit(limit)}
+
+
+if __name__ == '__main__':
+    from pprint import pprint
+    # print(UserAgent({
+    #     "software": [
+    #         'Android Browser 4.0'
+    #     ]
+    # }))
+    pprint(UserAgentGroups('engine', 5))
